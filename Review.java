d443eb2224a57c8d116115c50f35afb428237cc0
masaraed <30595626+masaraed@users.noreply.github.com>
2017-08-28 22:06:33 +0300

    Initial commit

---
+++ Review.java
@@ -0,0 +1,8 @@
+package com.example.android.popmovies;
+
+/**
+ * Created by masa on 8/26/17.
+ */
+
+public class Review {
+}
---
+++ TrailerAdapter.java
@@ -0,0 +1,8 @@
+package com.example.android.popmovies;
+
+/**
+ * Created by masa on 8/27/17.
+ */
+
+public class TrailerAdapter {
+}
